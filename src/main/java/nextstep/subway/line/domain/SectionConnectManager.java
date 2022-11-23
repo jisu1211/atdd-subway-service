be87f5919a33383279cb1d6b9bd0e55dbfa77520
jisu1211 <dbdnzl1211@gmail.com>
2022-11-23 21:30:26 +0900

    refactor: 1단계 피드백 반영

--- src/main/java/nextstep/subway/line/domain/SectionConnectManager.java
+++ src/main/java/nextstep/subway/line/domain/SectionConnectManager.java
@@ -1,28 +1,16 @@
 package nextstep.subway.line.domain;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.Set;
-
-import static java.util.stream.Collectors.toSet;
 
 public class SectionConnectManager {
     public static void connectAll(Line line, Section request, List<Section> matchedSections) {
-        Set<Section> requests = matchedSections.stream()
-                .map(section -> SectionConnectManager.connect(section, request))
-                .filter(Objects::nonNull)
-                .collect(toSet());
+        matchedSections.forEach(section -> SectionConnectManager.connect(section, request));
 
-        addNewSections(line, requests);
+        line.addSection(request);
     }
 
-    private static Section connect(Section current, Section request) {
+    private static void connect(Section current, Section request) {
         ConnectionType connectionType = ConnectionType.match(current, request);
         connectionType.connect(current, request);
-        return request;
-    }
-
-    private static void addNewSections(Line line, Set<Section> requests) {
-        requests.forEach(request -> line.addSection(request));
     }
 }
